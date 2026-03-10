f11fdd7e1f3b588b3bfd9c218c447e9816b8007b
VladaDemyk <vlada.demydenko.ri.2025@lpnu.ua>
2026-03-10 19:43:28 +0200

    Ініціалізація проєкту

---
+++ src/main/java/com/futura/FuturaFlow/FuturaFlowApplication.java
@@ -0,0 +1,13 @@
+package com.futura.FuturaFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuturaFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuturaFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/futura/FuturaFlow/FuturaFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futura.FuturaFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuturaFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
